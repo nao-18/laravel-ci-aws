de34ec9445d6ac71f1f6385a016b3d2d3e606a34
nao-18 <kyahyo17@yahoo.co.jp>
2020-09-20 22:36:17 +0900

    ログイン状態テスト追加

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,5 +16,18 @@ class ArticleControllerTest extends TestCase
         $response = $this->get(route('articles.index'));
 
         $response->assertStatus(200)->assertViewIs('articles.index');
+    }
+
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get(route('articles.create'));
+        $response->assertStatus(200)->assertViewIs('articles.create');
     }
 }
